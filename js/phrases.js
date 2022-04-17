eec6e5c5d81518800db8907a032c322cb2f07cfb
Hyago Vieira <dev.hyago@outlook.com>
2022-04-17 06:40:18 -0300

    starting game logic

---
+++ js/phrases.js
@@ -0,0 +1,19 @@
+const team = {
+   easy: ['SAO PAULO', 'FLUMINENSE', 'CORITIBA', 'VASCO', 'BOTAFOGO', 'PALMEIRAS'],
+   medium: ['BARCELONA', 'REAL MADRID', 'LIVERPOOL', 'MANCHESTER CITY', 'CHELSEA'],
+   hard: ['OLIMPIA', 'COLO COLO', 'TREZE']
+}
+
+const fruit = {
+   easy: ['BANANA', 'MELANCIA', 'MORANGO'],
+   medium: ['JABOTICABA', 'ACELORA'],
+   hard: ['CIRIGUELA', 'JAMBO']
+}
+
+const movie = {
+   easy: ['HULK', 'AQUAMAN', 'AFTER', 'CORINGA', 'TITANIC'],
+   medium: ['V DE VINGANÇA', 'THE BATMAN', 'MATRIX'],
+   hard: ['JANGO LIVRE', 'A FERA']
+}
+
+let phrase = [team, fruit, movie];
--- js/script.js
+++ js/script.js
@@ -1,1 +1,91 @@
-console.log("Hello word");
+let difficulty;
+let displaySelectLevel = document.querySelector('#select-level');
+let displayGame = document.querySelector('#game');
+let divTip = document.querySelector('#tip');
+let tip;
+let selectedPhrase;
+let displayPhrase = document.querySelector('.phrase');
+let correctPlayCount = 0;
+
+
+document.querySelector("#difficulty").addEventListener('click', () => {
+   let select = document.querySelector("#level");
+   difficulty = select.options[select.selectedIndex].value;
+   onDisplayGame();
+   selectedPhrase = sortPhase();
+   displayLetters(selectedPhrase);
+   divTip.innerHTML = `DICA: ${tip}`;
+   checkIfLetter(selectedPhrase);
+   console.log(selectedPhrase);
+});
+
+function checkIfLetter(str){
+
+   let divLetter = document.querySelectorAll('.letter');
+   let letters = document.querySelectorAll('.button');
+
+   letters.forEach(element => {
+      element.addEventListener('click', () => {
+         let lettersSelected = element.innerHTML;
+
+         str.split('').forEach((element, index) => {
+            if(lettersSelected == element){
+               divLetter[index].innerHTML = element;
+               
+               
+            
+            }
+         });
+
+      });
+   })
+}
+
+
+
+function displayLetters(str){
+   let createDiv = document.createElement('div');
+   createDiv.classList.add('letter');
+   for(let i = 0; i < str.length; i++){
+      let cloneDiv = createDiv.cloneNode(false);
+      if(str[i] == " "){
+         cloneDiv.style.border = 'none';
+         displayPhrase.appendChild(cloneDiv);
+         correctPlayCount++;
+      }
+      displayPhrase.appendChild(cloneDiv);
+   }
+}
+
+function sortPhase(){
+   let positionArr = randomNumber(phrase);
+   switch(positionArr){
+      case 0:
+         tip = "TIME";
+         break;
+      case 1:
+         tip = "FRUTA"
+         break;
+      case 2:
+         tip = "FILME"
+         break;
+      default:
+         tip = "ERROR";
+   }
+   let sortArrPhrase = phrase[positionArr][difficulty];
+   return sortArrPhrase[randomNumber(sortArrPhrase)];
+}
+
+function onDisplayGame(){
+   displaySelectLevel.style.display = 'none';
+   displayGame.style.display = 'flex';
+}
+
+function exitDisplayGame(){
+   displaySelectLevel.style.display = 'flex';
+   displayGame.style.display = 'none';
+}
+
+function randomNumber(arr){
+   return Math.floor(Math.random() * arr.length)
+}
